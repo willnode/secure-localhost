0ddd002033c81ce840757b55b49e9cb25c52dc2d
Wildan M <willnode@wellosoft.net>
2021-11-15 05:33:02 +0700

    Add mac & linux os (not tried yet)

--- src/approve.js
+++ src/approve.js
@@ -4,12 +4,21 @@ var path = require('path');
 module.exports = function (key, cert) {
     return new Promise(function (resolve, reject) {
         if ((fs.existsSync(key) && fs.existsSync(cert))) {
-            var child = require('child_process').spawn('certutil', ['-addstore', '-user', 'root', cert]);
+            var spawn = require('child_process').spawn;
+            let child;
+            switch (process.platform) {
+                case 'win32':
+                    child = spawn('certutil', ['-addstore', '-user', 'root', cert]);
+                case 'darwin':
+                    child = spawn('security', ['add-trusted-cert', '-d', '-r', 'trustRoot', '-k', '/Library/Keychains/System.keychain', cert]);
+                case 'linux':
+                    child = spawn('certutil', ['certutil', '-A', '-d', 'sql:~/.pki/nssdb', '-t', 'C', '-n', 'Certificate Common Name', '-i', cert]);
+            }
             child.stdout.on('data', function (data) {
-               // process.stdout.write(data);
+                // process.stdout.write(data);
             });
             child.stderr.on('data', function (data) {
-              //  process.stderr.write(data);
+                //  process.stderr.write(data);
             });
             child.on('close', function (code) {
                 if (code === 0) {
